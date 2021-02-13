9ccb171f04fa0968d583ba32b5c8c48ee4a39875
McKenzie <mac.gray.311@gmail.com>
2021-02-13 14:23:32 -0800

    Implementing Manhattan distance function.

--- ML/Utils.cs
+++ ML/Utils.cs
@@ -7,6 +7,9 @@ namespace ML
         #region Distance Functions
         public static double EuclideanDistance(double[] p1, double[] p2)
         {
+            if (p1.Length != p2.Length)
+                throw new ArgumentException($"{nameof(p1)} and {nameof(p2)} are not the same size.");
+
             double sum = 0;
             for (int i = 0; i < p1.Length; ++i)
             {
@@ -19,7 +22,16 @@ namespace ML
 
         public static double ManhattanDistance(double[] p1, double[] p2)
         {
-            throw new NotImplementedException();
+            if (p1.Length != p2.Length)
+                throw new ArgumentException($"{nameof(p1)} and {nameof(p2)} are not the same size.");
+
+            double distance = 0;
+            for (int i = 0; i < p1.Length; ++i)
+            {
+                distance += Math.Abs(p1[i] - p2[i]);
+            }
+
+            return distance;
         }
         #endregion
     }
